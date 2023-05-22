8018841de0ae155aeee749f5042d45e730bae2d0
Dmitriy <dmed832@yandex.ru>
2023-05-22 22:58:04 +0300

    добавили сервисный слой для User

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    void createUserTable();
+
+    void dropUserTable();
+
+    void saveUser(String name, String lastName, byte age);
+
+    void removeUserById(long id);
+
+    List<User> getAllUsers();
+
+    void clearUserTable();
+}
