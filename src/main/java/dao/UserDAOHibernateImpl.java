c42b66697f52ec1ab3b6667791a17f3d3f381a4c
Dmitriy <dmed832@yandex.ru>
2023-05-27 00:59:59 +0300

    implemented connection to the database through hibernate

---
+++ src/main/java/dao/UserDAOHibernateImpl.java
@@ -0,0 +1,102 @@
+package dao;
+
+import model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import util.Util;
+
+import java.util.List;
+
+public class UserDAOHibernateImpl implements UserDAO {
+
+    private final SessionFactory sessionFactory = Util.getSessionFactory();
+
+    @Override
+    public void createUserTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS user (id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(45), lastName VARCHAR(45), age TINYINT(3))";
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void dropUserTable() {
+        String sql = "DROP TABLE IF EXISTS user";
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(new User(name, lastName, age));
+            transaction.commit();
+            System.out.println("User с именем" + name + " фамилией " + lastName + " добавлен в базу данных");
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> userList;
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            userList = session.createQuery("from User").list();
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(e);
+        }
+        return userList;
+    }
+
+    @Override
+    public void clearUserTable() {
+        String sql = "TRUNCATE TABLE user";
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
