87522047d28c1933effa1d6300aea994dac020a9
Dmitriy <dmed832@yandex.ru>
2023-05-26 23:56:58 +0300

    map the User class to a table in the database

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -2,12 +2,27 @@ package model;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column
     private String name;
+    @Column
     private String lastName;
+    @Column
     private byte age;
+
+    public User(String name, String lastName, byte age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
 }
