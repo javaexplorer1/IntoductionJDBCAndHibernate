31bf525770ddbd4c5eac50364ce26693cf7882fe
Dmitriy <dmed832@yandex.ru>
2023-05-28 00:02:28 +0300

    added three hibernate configuration methods

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,13 +1,23 @@
 package util;
 
+import model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/test";
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "root";
+
+    private static SessionFactory sessionFactory;
 
     private Util() {
     }
@@ -24,5 +34,61 @@ public class Util {
         return connection;
     }
 
+    public static SessionFactory getHibernateConnection(){
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties properties = new Properties();
+                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                properties.put(Environment.URL, URL);
+                properties.put(Environment.USER, USER_NAME);
+                properties.put(Environment.PASS, PASSWORD);
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                properties.put(Environment.SHOW_SQL, "true");
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                properties.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(properties);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+
+    public static SessionFactory getSessionFactoryProperties() {   //hibernate.properties (in resources)
+        if (sessionFactory == null) {
+            try { Configuration configuration = new Configuration();
+                configuration.addAnnotatedClass(User.class);
+
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
+                builder.applySettings(configuration.getProperties());
+
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory() {         //hibernate.cfg.xml  (in resources)
+        try{
+            Configuration configuration = new Configuration().configure();
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
+    }
 
 }
