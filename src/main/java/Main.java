d1586c77222e21aca01335229179899812dbdd79
Dmitriy <dmed832@yandex.ru>
2023-05-31 22:45:18 +0300

    добавили рабочий метод для работы с hibernate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import dao.UserDAO;
 import dao.UserDAOJDBCImpl;
 import model.User;
 import service.UserService;
+import service.UserServiceHibernateImpl;
 import service.UserServiceJDBCImpl;
 import util.Util;
 
@@ -10,13 +11,28 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        scryptJDBC();
+        //scryptJDBC();
+        scryptHibernate();
 
     }
 
     private static void scryptJDBC() {
         Util.getJDBCConnection();
         UserService userService = new UserServiceJDBCImpl();
+        userService.createUserTable();
+        userService.saveUser("Андрей", "Иванов", (byte) 25);
+        userService.saveUser("name1", "lastName1", (byte) 35);
+        userService.saveUser("name2", "lastName2", (byte) 45);
+        userService.saveUser("name3", "lastName3", (byte) 65);
+        List<User> allUsers = userService.getAllUsers();
+        allUsers.forEach(System.out::println);
+        userService.clearUserTable();
+        userService.dropUserTable();
+    }
+
+    private static void scryptHibernate() {
+        Util.getHibernateConnection();
+        UserService userService = new UserServiceHibernateImpl();
         userService.createUserTable();
         userService.saveUser("Андрей", "Иванов", (byte) 25);
         userService.saveUser("name1", "lastName1", (byte) 35);
